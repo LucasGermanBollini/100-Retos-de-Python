5514555eae5f30fe190a41284870f3dd4429b25a
Lucas <89327322+LucasGermanBollini@users.noreply.github.com>
2024-03-07 03:41:33 -0300

    Update Aspect Ratio de una Imagen en Python.py

--- Aspect Ratio de una Imagen en Python.py
+++ Aspect Ratio de una Imagen en Python.py
@@ -1,3 +1,14 @@
+"""
+/*
+ * Crea un programa que se encargue de calcular el aspect ratio de una
+ * imagen a partir de una url.
+ * - Url de ejemplo:
+ *   https://raw.githubusercontent.com/mouredevmouredev/master/mouredev_github_profile.png
+ * - Por ratio hacemos referencia por ejemplo a los "16:9" de una
+ *   imagen de 1920*1080px.
+ */
+"""
+
 from PIL import Image
 import requests
 from io import BytesIO
